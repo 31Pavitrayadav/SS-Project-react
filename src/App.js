2f2ab36ffd6fe7b0b0fa9d3a210ecd8db3e216f4
31Pavitrayadav <pavitrayadavp@gmail.com>
2024-07-02 04:27:19 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div classname="App-header">
+      Pavitra Yadav
+      </div>
+      </div>
   );
 }
-
+function Demo() {
+  return (
+    <div className="App">
+    <div classname="App-header">
+      <Demo></Demo>
+      </div>
+      </div>
+  );
+}
+    
 export default App;
+    
+//demo is used as an html tags
